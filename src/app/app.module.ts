7f257ad637e0177f20eebedc28bb5fe893af8a3b
Deepika Singh <d.108singh@gmail.com>
2024-04-15 23:56:40 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookDetailsComponent
+    BookDetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
